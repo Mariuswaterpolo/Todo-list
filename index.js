9e032968f70664fb8ee06a92971251ee6066a819
Mariuswaterpolo <57652140+Mariuswaterpolo@users.noreply.github.com>
2019-11-13 11:07:49 -0800

    I just added remove function

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ function addNewToDo()
    var newElement = event.target.newToDo.value;
    var newElementInTheList = document.createElement('li');
    newElementInTheList.innerHTML = newElement;
+   newElementInTheList.setAttribute('onclick', 'removeTodo()');
    list.appendChild(newElementInTheList);
    inputSubmit.value = "";
 }
     newToDoInputvalue = "";
 
-
+function removeTodo()
+{
+   var el = event.target;
+   var parentEl = el.parentNode;
+   parentEl.removeChild(el);
+}
 
 
 
